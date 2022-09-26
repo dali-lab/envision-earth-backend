ab1631a2a73983fb00d89ef867afd5f4e7664025
rishavchakra <rishavchakra@gmail.com>
2022-09-26 19:38:53 -0400

    Photo Service finish

--- src/validation/photo.ts
+++ src/validation/photo.ts
@@ -1,20 +1,21 @@
-// // import joi from 'joi';
-// import { ValidatedRequestSchema, ContainerTypes } from 'express-joi-validation';
-// import { IPhoto } from '../db/models/photo';
-
-// export interface CreatePhotoRequest extends ValidatedRequestSchema {
-//   [ContainerTypes.Body]: IPhoto
-// }
-
-// export interface UpdatePhotoRequest extends ValidatedRequestSchema {
-//   [ContainerTypes.Body]: Partial<IPhoto>
-// }
-
 import joi from 'joi';
-import { ContainerTypes, ValidatedRequest, ValidatedRequestSchema } from 'express-joi-validation'
+import { ContainerTypes, ValidatedRequestSchema } from 'express-joi-validation'
+import { IPhoto } from 'db/models/photo'
 
-interface PhotoRequestSchema extends ValidatedRequestSchema {
-  [ContainerTypes.Body]: {
-
-  },
+export interface CreatePhotoRequest extends ValidatedRequestSchema {
+  [ContainerTypes.Body]: IPhoto
 }
+
+export interface UpdatePhotoRequest extends ValidatedRequestSchema {
+  [ContainerTypes.Body]: Partial<IPhoto>
+}
+
+export const CreatePhotoSchema = joi.object<IPhoto>({
+  fullUrl: joi.string().required().error(() => 'Create Photo expectin a URL'),
+});
+
+export const UpdatePhotoSchema = joi.object<IPhoto>({
+  // fullUrl: joi.strin().required().error(() => 'Create Photo expecting a URL'),
+  id: joi.string().error(() => 'Update Photo expecting an ID'),
+  fullUrl: joi.string().error(() => 'Update Photo expecting a link'),
+});
