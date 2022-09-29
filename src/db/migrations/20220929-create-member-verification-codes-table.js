e115bc9e7925725a9326d32b9518b993133dfb3b
rishavchakra <rishavchakra@gmail.com>
2022-09-29 18:21:10 -0400

    Member verification codes table

---
+++ src/db/migrations/20220929-create-member-verification-codes-table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('membership_verification_codes', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+      },
+      membershipId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'memberships',
+          key: 'id',
+        },
+      },
+      code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      expiration: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('photos');
+  }
+}
